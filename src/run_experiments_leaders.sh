cac0ed9e4b37b02f8a71ce9b398ffef14e743d64
Jacopo Lenti <jacopo.lenti@megazord.centai.eu>
2025-03-18 08:28:42 +0000

    scripts and notebooks

--- src/run_experiments_leaders.sh
+++ src/run_experiments_leaders.sh
@@ -8,6 +8,7 @@ date="240430_2flow"
 initial_leaders_ratio="5 10 25 50 100"
 method="svinormal sviNF mcmc abc"
 
-rep="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19"
+rep="12 13 14 15 16 17 18 19"
+# rep="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19"
 
 parallel --timeout ${timeout} --ungroup --max-procs ${max_procs} "python inference_leaders.py {1} {2} {3} {4} {5} {6}" ::: $rep ::: $T ::: $N ::: $initial_leaders_ratio ::: $method ::: $date
